a713b6418fd77a727c151b0ef0e0f981e554fb3d
Rami Massoud <ramimassoud@users.noreply.github.com>
2015-10-20 10:53:31 -0400

    Allow using binaries through rbenv

--- init.el
+++ init.el
@@ -4,6 +4,21 @@
 (pallet-mode t)
 (add-to-list 'load-path "~/.emacs.d/custom")
 
+(defmacro after (mode &rest body)
+  "`eval-after-load' MODE evaluate BODY.
+This allows us to define configuration for features that aren't
+always installed and only eval that configuration after the feature is loaded.
+ELPA packages usually provide an -autoloads feature which we can
+use to determine if the package is installed/loaded."
+  (declare (indent defun))
+  `(eval-after-load (symbol-name ,mode)
+     '(progn ,@body)))
+
+(setq rbenv-installation-directory "/usr/local/bin/rbenv")
+(after 'rbenv-autoloads
+ (require 'rbenv)
+ (global-rbenv-mode))
+
 (load "00personal-setup.el")
 (load "01ruby.el")
 (load "02keybindings.el")
