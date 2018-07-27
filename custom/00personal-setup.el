a924bf79a11aa01648ed300ef4cac05fec375793
Rami Massoud <rami.massoud@blueapron.com>
2018-07-26 23:23:02 -0400

    Update autocomplete dir

--- custom/00personal-setup.el
+++ custom/00personal-setup.el
@@ -184,7 +184,7 @@
 
  (require 'auto-complete-config)
  (add-to-list 'ac-dictionary-directories
-     "~/.emacs.d/.cask/24.5.1/elpa/auto-complete-20150408.1132/dict")
+     "~/.emacs.d/.cask/26.1/elpa/auto-complete-20170124.1845/dict")
  (ac-config-default)
  (setq ac-ignore-case nil)
  (add-to-list 'ac-modes 'enh-ruby-mode)
