9c1a53af696f09cd27193c3c76759036e258475f
Rami Massoud <rami.massoud@blueapron.com>
2018-07-26 23:31:51 -0400

    Hard code cask path until I can find a better fix

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(require 'cask)
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
